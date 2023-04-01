ea164f988eee4e458b52dbb51aeef64b5ced3341
lands69 <105416854+lands69@users.noreply.github.com>
2023-04-01 12:06:10 +0800

    Update buster.py

--- buster.py
+++ buster.py
@@ -7,15 +7,19 @@ agentid = ""
 secret = ""
 
 cookie = ""
-
-url = "https://www.acgcbk.vip/api/v1/actions/daily_sign"
+domain = "https://www.acgcbk.vip/"
+sigin_url = "https://www.acgcbk.vip/api/v1/actions/daily_sign"
 headers = {
     "referer": "https://www.acgcbk.vip/me/credits",
     "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
     "cookie": cookie
     }
-data = {"_wpnonce": "45c0cf50c7"}
-content = requests.post(url,headers=headers,data=data).json()['message']
+nonce_resp = requests.get(domain,headers=headers).text
+t = etree.HTML(nonce_resp)
+nonce = eval(t.xpath('/html/head/script[1]/text()')[0].split("=")[-1])['_wpnonce']
+
+data = {"_wpnonce": nonce}
+content = requests.post(sigin_url,headers=headers,data=data).json()['message']
 
 s = requests.get(f"https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={corpid}&corpsecret={secret}")
 token = s.json().get("access_token",False)
